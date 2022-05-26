78a3b1bf5353f8bfc78fc7418fd53f18d3f8f846
navneetgoel <goel.navneet4@gmail.com>
2022-05-25 17:46:39 -0700

    Added db

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const cache = require('memory-cache');
 
 const PORT = process.env.PORT || 3128;
 const app = express();
+
+
+// cache midlleware
+
+// app routes
+app.get('/products', (req, res) => {
+    setTimeout(() => {
+        let db = new sqlite3.Database("./NodeInventory.db");
+        let sql = "Select * from products";
+
+        db.all(sql, [], (err, rows) => {
+            if (err){
+                throw err;
+            }
+            db.close();
+            res.send(rows);
+        });
+    }, 3000);
+})
+
+app.listen(PORT, () => {
+    console.log(`Application is running on Port: ${PORT}`);
+});
