bd13774f06604878d6baa9ed0962bae9ad37bdf7
navneetgoel <goel.navneet4@gmail.com>
2022-05-26 10:17:40 -0700

    Added flat cache

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cache = require('memory-cache');
 const flatCache = require('flat-cache');
+const path = require("path");
 
 const PORT = process.env.PORT || 3128;
 const app = express();
 
-let cache = flatCache.load('productCache');
+
 
 
 // cache midlleware
@@ -30,8 +31,26 @@ let cacheMiddleware = (duration) => {
     }
 }
 
+// flat cache
+let flat_cache = flatCache.load('productCache', path.resolve("./cache"));
+let flatCacheMiddleware = (req, res, next) => {
+    let key = '__express__'+ req.originalUrl || req.url;
+    let cacheContent = flat_cache.getKey(key);
+    if (cacheContent){
+        res.send(cacheContent);
+    } else {
+        res.sendResponse = res.send
+        res.send = (body) => {
+            flat_cache.setKey(key, body);
+            flat_cache.save();
+            res.sendResponse(body);
+        }
+        next();
+    }
+}
+
 // app routes
-app.get('/products', cacheMiddleware(30), (req, res) => {
+app.get('/products', flatCacheMiddleware, (req, res) => {
     setTimeout(() => {
         let db = new sqlite3.Database("./NodeInventory.db");
         let sql = "Select * from products";
